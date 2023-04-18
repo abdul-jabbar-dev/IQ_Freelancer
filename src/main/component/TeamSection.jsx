781be12b1ab40f07b44205030689e2de58bc6673
Maruf Mobin <mobinh264@gmail.com>
2023-04-19 03:40:08 +0600

    Team section Done

--- src/main/component/TeamSection.jsx
+++ src/main/component/TeamSection.jsx
@@ -14,15 +14,15 @@ const TeamSection = () => {
         <section className='team-section'>
             <Container>
                 <Row>
-                    <h1>Meet our experts always ready to help you</h1>
+                    <Col><h1 className='team-section-heading pb-5'>Meet our experts always ready to help you</h1></Col>
                 </Row>
                 <Row>
                     {
                         fetchTeamDatas?.map(fetchTeamData => <Col key={fetchTeamData?.id}>
-                            <div>
-                                <img src={fetchTeamData?.img} alt="" />
-                                <h4>{fetchTeamData?.name}</h4>
-                                <h6>{fetchTeamData?.designation}</h6>
+                            <div className='text-center team-section-inner'>
+                                <img src={fetchTeamData?.img} alt="" className='img-fuied'/>
+                                <h4 className='team-section-name'>{fetchTeamData?.name}</h4>
+                                <h6 className='team-section-designation'>{fetchTeamData?.designation}</h6>
                             </div>
                         </Col>)
                     }
